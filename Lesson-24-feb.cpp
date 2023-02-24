cfbe227851e22af27f89b15c13db9e4a74e02274
Max BeslanPC <max@huzm.ru>
2023-02-24 14:38:50 +0300

    Init commit

---
+++ Lesson-24-feb.cpp
@@ -0,0 +1,18 @@
+﻿// Учебный проект для занатия
+// от 24 февраля 2023 г.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void show_prog(int start, int step, int size) {
+    int i, x;
+    for (i = 0, x = start; i < size; i++, x += step)
+        cout << x << ' ';
+    return 0;
+}
+
+int main()
+{
+    show_prog(0, 5, 10);
+}
